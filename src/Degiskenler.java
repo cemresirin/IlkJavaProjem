a5792d84bafd10c92416270f4e609998357171dd
cemresirin <cemre25355@gmail.com>
2025-05-23 23:36:46 +0300

    İlk Java projem yüklendi

---
+++ src/Degiskenler.java
@@ -0,0 +1,20 @@
+public class Degiskenler {
+public static void main(String[] args )  {
+        int yas = 26;
+        double maas = 15000.0;
+        float sıcaklık = 36.5f;
+        boolean evliMi = true;
+        char cinsiyet = 'K';
+        String isim = "Cemre";
+
+        final int DOGUM_YILI = 1999;
+
+        System.out.println("İsim: " + isim);
+        System.out.println("Yaş: " + yas);
+        System.out.println("Maaş: " + maas);
+        System.out.println("Sıcaklık: " + sıcaklık);
+        System.out.println("Evli mi?: " + evliMi);
+        System.out.println("Cinsiyet: " + cinsiyet);
+        System.out.println("Doğum Yılı: " + DOGUM_YILI);
+    }
+}
