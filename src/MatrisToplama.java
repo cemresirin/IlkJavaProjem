1a1084af2445c3064ec35d49c6bdc2f3d0087932
CEMRE <cemre25355@gmail.com>
2025-05-25 11:51:50 +0300

    MatrisToplama.java

--- src/MatrisToplama.java
+++ src/MatrisToplama.java
@@ -1,27 +1,9 @@
-import java.util.Scanner;
-
 public class MatrisToplama {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        int[][] matris1 = new int[2][2];
-        int[][] matris2 = new int[2][2];
+        int[][] matris1 = { {1,2}, {3,4} };
+        int[][] matris2 = { {5,6}, {7,8} };
         int[][] toplamMatris = new int[2][2];
 
-        System.out.println("1.matrisi girin (2x2):");
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                System.out.print("matris1[" + i + "][" + j + "]: ");
-                matris1[i][j] = input.nextInt();
-            }
-        }
-        System.out.println("2.Matrisi girin (2x2):");
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                System.out.print("matris2[" + i + "][" + j + "]: ");
-                matris2[i][j] = input.nextInt();
-            }
-        }
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
                 toplamMatris[i][j] = matris1[i][j] + matris2[i][j];
@@ -29,11 +11,11 @@ public class MatrisToplama {
         }
         System.out.println("Toplam Matris:");
         for (int i = 0; i < 2; i++) {
+            System.out.print("| ");
             for (int j = 0; j < 2; j++) {
                 System.out.print(toplamMatris[i][j] + " ");
             }
-            System.out.println();
+            System.out.println("|");
         }
-        input.close();
     }
 }
